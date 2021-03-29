c3f4d2f057635322cf00812f63fffe517a096b2f
Tori FavWest <practiceandthenpractice@gmail.com>
2021-03-29 16:30:23 -0700

    make results display as a list
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 // return a list of all the unique words and how many times they appeared
-
-
 $(document).ready(function(){
   //get the paragraph
   $("#form1").submit(function(event){
@@ -10,28 +8,30 @@ $(document).ready(function(){
     const roughAllWords = paragraph.split(" ");
     let allWords=[];
     roughAllWords.forEach(function(word){
-      if(word.length>0){
+      if (word.length > 0) {
         allWords.push(word);
       }
     });
     //create a second array and add each word to it. If the word is already on the list, update it instead
     let onceWords=[];
     allWords.forEach(function(word){
-      if(!onceWords.includes(word)){
+      if (!onceWords.includes(word)) {
         onceWords.push(word);
       }
     });
     onceWords.forEach(function(once_word){
       count = 0;
       allWords.forEach(function(word){
-        if(once_word===word){
+        if (once_word === word) {
           count ++ ;
         }
       });
-      onceWords.splice(onceWords.indexOf(once_word), 1, once_word + " " + count);
+      onceWords.splice(onceWords.indexOf(once_word), 1, once_word + ": " + count + " ");
     });
-    console.log(onceWords);
+    onceWords.forEach(function(word) {
+    $(".words").append("<li>" + word + "</li>") });
+    $(".result").show();
+    });
   });
-});
 
 
